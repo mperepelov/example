ed8695d18d417ebf92dc1f712de37144fbb61299
Max Perepelov <c-mperepelov@lendingclub.com>
2018-08-22 21:18:09 +0300

    Change webdriver to chrome

--- sample.py
+++ sample.py
@@ -1,26 +1,10 @@
 import time
-
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
 
-def execute_with_retry(method, max_attempts):
-    e = None
-    for i in range(0, max_attempts):
-        try:
-            return method()
-        except Exception as e:
-            print(e)
-            time.sleep(1)
-    if e is not None:
-        raise e
-
-capabilities = DesiredCapabilities.FIREFOX
-capabilities["marionette"] = True
-firefox_bin = "/usr/bin/firefox"
-browser = execute_with_retry(lambda: webdriver.Firefox(
-    firefox_binary=firefox_bin, capabilities=capabilities), 10)
-
-browser.get("https://www.google.com")
+driver.get('http://www.google.com/xhtml');
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
 
 time.sleep(10)
 
